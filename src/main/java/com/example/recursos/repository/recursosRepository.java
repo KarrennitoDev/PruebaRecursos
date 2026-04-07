76e72f69df2c8123c1020aa44de2a8a9bfd2e284
KarrennitoDev <a.leoncarreno17@gmail.com>
2026-04-06 20:05:39 -0400

    Creacion de lista dentro del repository

--- src/main/java/com/example/recursos/repository/recursosRepository.java
+++ src/main/java/com/example/recursos/repository/recursosRepository.java
@@ -1,4 +1,17 @@
 package com.example.recursos.repository;
 
+import com.example.recursos.model.recursosModel;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
 public class recursosRepository {
+
+    private List<recursosModel> listaRecursos = new ArrayList<>();
+
+    public List<recursosModel> findAll() {
+        return listaRecursos;
+    }
 }
