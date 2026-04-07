e09401103b7387f6f8c67683581890127c69e434
KarrennitoDev <a.leoncarreno17@gmail.com>
2026-04-06 20:11:44 -0400

    Se crean las funciones para mostrar, agregar, buscar y eliminar

--- src/main/java/com/example/recursos/repository/recursosRepository.java
+++ src/main/java/com/example/recursos/repository/recursosRepository.java
@@ -5,13 +5,14 @@ import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public class recursosRepository {
 
     private List<recursosModel> listaRecursos = new ArrayList<>();
 
-    public List<recursosModel> findAll() {
+    public List<recursosModel> mostrar() {
         return listaRecursos;
     }
 
@@ -19,5 +20,21 @@ public class recursosRepository {
         listaRecursos.add(recurso);
     }
 
+    public recursosModel buscar(int id) {
+        for (recursosModel recurso : listaRecursos) {
+            if (recurso.getid() == id) {
+                return recurso;
+            }
+        }
+        return null;
+    }
 
+    public void eliminar(int id) {
+        for (int i = 0; i < listaRecursos.size(); i++) {
+            if (listaRecursos.get(i).getid() == id) {
+                listaRecursos.remove(i);
+                break;
+            }
+        }
+    }
 }
