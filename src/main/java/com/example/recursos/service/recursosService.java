360e680a30acf3ecadb9f06f29ffedc9ff31455d
KarrennitoDev <a.leoncarreno17@gmail.com>
2026-04-06 20:41:56 -0400

    Creacion de codigo de package Service con las funciones correspondientes

--- src/main/java/com/example/recursos/service/recursosService.java
+++ src/main/java/com/example/recursos/service/recursosService.java
@@ -1,4 +1,40 @@
 package com.example.recursos.service;
 
+import com.example.recursos.model.recursosModel;
+import com.example.recursos.repository.recursosRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class recursosService {
+
+    @Autowired
+    private recursosRepository repository;
+
+    public List<recursosModel> listarTodo() {
+        return repository.mostrar();
+    }
+
+    public void crear(recursosModel recurso) {
+        repository.guardar(recurso);
+    }
+
+    public void borrar(int id) {
+        repository.eliminar(id);
+    }
+
+    public List<recursosModel> rDisponibles() {
+        List<recursosModel> todos = repository.mostrar();
+        List<recursosModel> soloDisponibles = new ArrayList<>();
+
+        for (recursosModel r : todos) {
+            if (r.isdisponibilidad() == true) {
+                soloDisponibles.add(r);
+            }
+        }
+        return soloDisponibles;
+    }
 }
